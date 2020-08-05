3171b4fc77c7bafb4db9bc14ec9a9a64f98b0492
GitHub Action <action@github.com>
2020-08-05 01:24:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/lzcc2003/usefulplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lzcc2003/usefulplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lzcc2003.usefulplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lzcc2003.usefulplugin.MyBundle
 
 class MyApplicationService {
 
